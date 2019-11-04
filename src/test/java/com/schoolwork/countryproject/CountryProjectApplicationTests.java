2a1491ff1cd4f800e3039b4a1bcad9f9350bd98f
Justin Gent <justinbgent@gmail.com>
2019-11-04 14:33:31 -0700

    Create Country and CountryList classes with constructors

---
+++ src/test/java/com/schoolwork/countryproject/CountryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolwork.countryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
