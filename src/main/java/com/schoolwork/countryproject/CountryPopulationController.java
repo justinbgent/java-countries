ad6c336cba2298182f424b0eac97c04b93061f79
Justin Gent <justinbgent@gmail.com>
2019-11-04 16:42:59 -0700

    Add Country name and population functions and logic inside country name functions

--- src/main/java/com/schoolwork/countryproject/CountryPopulationController.java
+++ src/main/java/com/schoolwork/countryproject/CountryPopulationController.java
@@ -1,7 +1,26 @@
 package com.schoolwork.countryproject;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController("/population")
 public class CountryPopulationController {
+    @GetMapping(value = "/size/{people}", produces = {"application/json"})
+    public ResponseEntity<?> getCountriesWithPopulationAbove(@PathVariable long people){
+
+    }
+
+    @GetMapping(value = "/min", produces = {"application/json"})
+    public ResponseEntity<?> getLowestPopulationCountry(){
+
+    }
+
+    @GetMapping(value = "/max", produces = {"application/json"})
+    public ResponseEntity<?> getCountryWithGreatestPopulation(){
+
+    }
 }
